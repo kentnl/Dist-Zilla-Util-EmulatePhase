059d8451fb84b44df5dea72cff249b7ebd80e37b
Kent Fredric <kentfredric@gmail.com>
2010-12-06 03:40:35 +1300

    Add fix, but with warnings

--- lib/Dist/Zilla/Util/EmulatePhase/PrereqCollector.pm
+++ lib/Dist/Zilla/Util/EmulatePhase/PrereqCollector.pm
@@ -32,6 +32,22 @@ sub find_files {
   return shift->shadow_zilla->find_files( @_ );
 }
 
+## no critic ( Subroutines::RequireArgUnpacking )
+=method _share_dir_map
+
+L<< C<Dist::Zilla>'s C<_share_dir_map>|Dist::Zilla/_share_dir_map >> proxy.
+
+B<WARNING>: This method is provided as a temporary workaround and may eventually disappear,
+as the behaviour it is wrapping probably shouldn't be done like this.
+
+=cut
+sub _share_dir_map {
+  my $self = shift;
+  require Carp;
+  Carp::carp("[Dist::Zilla::Util::EmulatePhase] Call to \$self->zilla->_share_dir_map should be avoided");
+  return $self->shadow_zilla->_share_dir_map( @_ );
+}
+
 no Moose;
 __PACKAGE__->meta->make_immutable;
 1;
