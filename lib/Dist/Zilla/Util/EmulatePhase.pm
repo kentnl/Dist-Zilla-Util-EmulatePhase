86a899515cd0dafecd81b1c6be99c3e6047b194a
Kent Fredric <kentfredric@gmail.com>
2014-04-18 20:26:55 +1200

    Build results of bf72453 (on master)

--- lib/Dist/Zilla/Util/EmulatePhase.pm
+++ lib/Dist/Zilla/Util/EmulatePhase.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Util::EmulatePhase;
 $Dist::Zilla::Util::EmulatePhase::VERSION = '0.01025804';
-#ABSTRACT: Nasty tools for probing L<< C<Dist::Zilla>'s|Dist::Zilla >> internal state.
+#ABSTRACT: Nasty tools for probing Dist::Zilla's internal state.
 
 our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
@@ -45,7 +45,7 @@ sub deduplicate {
       my $a = refaddr($item);
       @out->push($item) unless %seen->exists($item);
       %seen->put( $item => 1 );
-    }
+    },
   );
   return @out;
 }
@@ -114,7 +114,7 @@ sub get_plugins {
       sub {
         my $with = expand_modname(shift);
         return $plugins->grep( sub { $_->does($with) } )->flatten;
-      }
+      },
     );
   }
 
@@ -124,7 +124,7 @@ sub get_plugins {
         my ( $index, $value ) = @_;
         my $without = expand_modname($value);
         $plugins = $plugins->grep( sub { not $_->does($without) } );
-      }
+      },
     );
   }
 
@@ -133,7 +133,7 @@ sub get_plugins {
       sub {
         my $isa = expand_modname(shift);
         return $plugins->grep( sub { $_->isa($isa) } )->flatten;
-      }
+      },
     );
   }
 
@@ -143,7 +143,7 @@ sub get_plugins {
         my ( $index, $value ) = @_;
         my $isnt = expand_modname($value);
         $plugins = $plugins->grep( sub { not $_->isa($isnt) } );
-      }
+      },
     );
   }
 
@@ -183,10 +183,10 @@ sub get_metadata {
   my $meta    = {};
   @plugins->each(
     sub {
-      my ( $index, $value ) = @_;
+      my ( undef, $value ) = @_;
       require Hash::Merge::Simple;
       $meta = Hash::Merge::Simple::merge( $meta, $value->metadata );
-    }
+    },
   );
   return $meta;
 }
@@ -238,7 +238,7 @@ sub get_prereqs {
         require Carp;
         Carp::croak('Zilla did not reset itself');
       }
-    }
+    },
   );
   $zilla->prereqs->finalize;
   return $zilla->prereqs;
@@ -254,7 +254,7 @@ __END__
 
 =head1 NAME
 
-Dist::Zilla::Util::EmulatePhase - Nasty tools for probing L<< C<Dist::Zilla>'s|Dist::Zilla >> internal state.
+Dist::Zilla::Util::EmulatePhase - Nasty tools for probing Dist::Zilla's internal state.
 
 =head1 VERSION
 
