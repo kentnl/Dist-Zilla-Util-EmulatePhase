5108ad8e4de8fd5dfeba6a91b1cb3fe01ba079fe
Kent Fredric <kentfredric@gmail.com>
2014-08-16 12:04:59 +1200

    v1.000002
     [00 Trivial]
     - CPANDAY!
     - no code changes.
     - augment tests.
     - update metadata
    
     [Dependencies::Stats]
     - Dependencies changed since 1.000001, see misc/*.deps* for details
     - configure: ↓1
     - develop: +1 (suggests: ↑1)
     - test: +1 -3 (recommends: ↑1 -1)
    
     [Metadata]
     - show 1x x_contributor
    
     [Tests]
     - update Test::ReportPrereqs

--- lib/Dist/Zilla/Util/EmulatePhase.pm
+++ lib/Dist/Zilla/Util/EmulatePhase.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Util::EmulatePhase;
 
-our $VERSION = '1.000002';
+our $VERSION = '1.000003';
 
 #ABSTRACT: Nasty tools for probing Dist::Zilla's internal state.
 
--- lib/Dist/Zilla/Util/EmulatePhase/PrereqCollector.pm
+++ lib/Dist/Zilla/Util/EmulatePhase/PrereqCollector.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Util::EmulatePhase::PrereqCollector;
 
-our $VERSION = '1.000002';
+our $VERSION = '1.000003';
 
 #ABSTRACT: A dummy Dist::Zilla to fake a 'prereq' object on.
 
