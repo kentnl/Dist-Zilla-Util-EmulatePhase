f01dcc371ec7577e8a70cbd54894d4b3b8859290
Kent Fredric <kentfredric@gmail.com>
2014-08-26 08:16:01 +1200

    Build results of a12b67a (on master)

--- lib/Dist/Zilla/Util/EmulatePhase/PrereqCollector.pm
+++ lib/Dist/Zilla/Util/EmulatePhase/PrereqCollector.pm
@@ -124,6 +124,19 @@ Dist::Zilla::Util::EmulatePhase::PrereqCollector - A dummy Dist::Zilla to fake a
 
 version 1.001000
 
+=head1 QUICK REFERENCE
+
+  A>shadow_zilla                  # Ref
+  A>prereqs                       # Object
+  ->register_prereqs              # mutator via prereqs
+  ->find_files                    # Proxy via ->shadow_zilla
+  ->plugins                       # []
+  ->_is_white_listed(options=[])  # Bool
+    0 =>  $caller_package
+    1 =>  $caller_subname
+
+  ->_share_dir_map()              # HashRef
+
 =head1 METHODS
 
 =head2 find_files
