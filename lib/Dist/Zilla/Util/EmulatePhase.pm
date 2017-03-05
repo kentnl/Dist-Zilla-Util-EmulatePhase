4cb58fe6e433f665ecd3d92b2133d41dbc1d7756
Kent Fredric <kentfredric@gmail.com>
2017-03-05 19:57:27 +1300

    v1.001002
     [Bugfix]
     - Avoid test failures on 5.26 w/ -Ddefault_inc_excludes_dot
     - Closes https://github.com/kentnl/Dist-Zilla-Util-EmulatePhase/issues/2
    
     [Dependencies::Stats]
     - Dependencies changed since 1.001001, see misc/*.deps* for details
     - configure: +1 (recommends: ↑1)
     - develop: +14 ↑4 -3 (recommends: +1, suggests: ↑2)
     - runtime: ↓1 -1 (recommends: +1)
     - test: +2 -1 (recommends: +1 ↑1 ↓1)
    
     [Tests]
     - Avoid use of dztest

--- lib/Dist/Zilla/Util/EmulatePhase.pm
+++ lib/Dist/Zilla/Util/EmulatePhase.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Util::EmulatePhase;
 
-our $VERSION = '1.001002';
+our $VERSION = '1.001003';
 
 #ABSTRACT: Nasty tools for probing Dist::Zilla's internal state.
 
--- lib/Dist/Zilla/Util/EmulatePhase/PrereqCollector.pm
+++ lib/Dist/Zilla/Util/EmulatePhase/PrereqCollector.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Util::EmulatePhase::PrereqCollector;
 
-our $VERSION = '1.001002';
+our $VERSION = '1.001003';
 
 #ABSTRACT: A dummy Dist::Zilla to fake a 'prereq' object on.
 
